e7afe8c2f371a0469eb19b5eef01cf78806c1e47
JC <jcsmileyjr@gmail.com>
2025-05-06 10:36:51 -0500

    Feature: Inital setup of nav UI elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className=" min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <nav className="flex justify-between">Hello World</nav>
+      <nav className="flex flex-row justify-between">
+        <ul className="flex flex-row gap-4">
+          <li>Home</li>
+          <li>Events</li>
+          <li>About</li>
+          <li>Sponsors</li>
+          <li>Community Partners</li>
+          <li>Testimonials</li>
+        </ul>
+        <h1>Memphis Tech Scene</h1>
+      </nav>
+      <main className="flex flex-col gap-8">Hello World</main>
     </div>
   );
 }
