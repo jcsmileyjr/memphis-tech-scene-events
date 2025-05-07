09746151468348d8a03045609c42cd42e7f9a3c5
JC <jcsmileyjr@gmail.com>
2025-05-07 01:41:41 -0500

    Style: Add user-interaction styling to links

--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -16,12 +16,12 @@ const Nav = () => {
     return (
         <nav className={`flex flex-row px-8 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
             <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-12 sm:pt-14 ml-4' : 'hidden'}`}>
-                <li className="bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)]">Home</li>
-                <li className="menu-item bg-white xl:bg-transparent">Events</li>
-                <li className="menu-item bg-white xl:bg-transparent">About</li>
-                <li className="menu-item bg-white xl:bg-transparent">Sponsors</li>
-                <li className="menu-item bg-white xl:bg-transparent">Collaborator</li>
-                <li className="menu-item bg-white xl:bg-transparent">Testimonials</li>
+                <li className=" cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)]">Home</li>
+                <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)]">Events</li>
+                <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)]">About</li>
+                <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)]">Sponsors</li>
+                <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)]">Collaborator</li>
+                <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)]">Testimonials</li>
             </ul>
             <Menu onClick={handleToggleMenu} className={`xl:hidden ${expanded ? 'hidden' : 'block'}`} />
             <h1 className="font-[family-name:var(--font-montserrat)] font-bold text-2xl sm:text-3xl xl:text-4xl z-50">Memphis Tech Scene</h1>
