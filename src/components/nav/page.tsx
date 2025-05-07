39e39933c2a7430f90a0c6935bdaeeae0f58c727
JC <jcsmileyjr@gmail.com>
2025-05-07 02:35:06 -0500

    Refactor: Highlight page links based on page web address

--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Menu } from 'lucide-react';
 import {useState} from 'react';
+import { usePathname } from 'next/navigation';
 import { useMediaQuery } from 'react-responsive';
 
-const Nav = ({pageLocation}: {pageLocation: string}) => {
+const Nav = () => {
+    const pageLocation = usePathname();
     const [expanded, setExpanded] = useState(false);
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1280px)' });
 
@@ -16,8 +18,8 @@ const Nav = ({pageLocation}: {pageLocation: string}) => {
     return (
         <nav className={`flex flex-row px-4 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
             <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-12 sm:pt-14 ml-4' : 'hidden'}`}>
-                <li className={`${pageLocation==='Home' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Home</li>
-                <li className={`${pageLocation==='Events' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Events</li>
+                <li className={`${pageLocation==='/' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Home</li>
+                <li className={`${pageLocation==='/events' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Events</li>
                 <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">About</li>
                 <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Sponsors</li>
                 <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Collaborator</li>
