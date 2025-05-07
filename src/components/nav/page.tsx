76d4a54921f9a7486e56911c2ce00ae3e6e72ba6
JC <jcsmileyjr@gmail.com>
2025-05-07 02:51:39 -0500

    Style: Additonal whitespace in Nav component in desktop view

--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -16,7 +16,7 @@ const Nav = () => {
     }
 
     return (
-        <nav className={`flex flex-row px-4 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
+        <nav className={`flex flex-row px-4 xl:px-12 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
             <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-12 sm:pt-14 ml-4' : 'hidden'}`}>
                 <li className={`${pageLocation==='/' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Home</li>
                 <li className={`${pageLocation==='/events' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Events</li>
