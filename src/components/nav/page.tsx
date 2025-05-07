0e7ef5d38e128922f07d425fd060616371911e86
JC <jcsmileyjr@gmail.com>
2025-05-07 00:37:44 -0500

    Refactor: Remove test UI elements

--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -6,13 +6,12 @@ const Nav = () => {
     const [expanded, setExpanded] = useState(false);
 
     const handleToggleMenu = () => {
-        console.log(expanded);
         setExpanded(!expanded);
     }
 
     return (
         <nav className={`flex flex-row  ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
-            <ul className="hidden lg:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)]">
+            <ul onClick={handleToggleMenu} className={`lg:hidden lg:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-white pt-8' : 'hidden'}`}>
                 <li className="text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)]">Home</li>
                 <li className="menu-item">Events</li>
                 <li className="menu-item">About</li>
@@ -20,15 +19,9 @@ const Nav = () => {
                 <li className="menu-item">Collaborator</li>
                 <li className="menu-item">Testimonials</li>
             </ul>
-            <ul onClick={handleToggleMenu} className={`lg:hidden ${expanded ? ' absolute top-0 left-0 z-50 bg-white' : 'hidden'}`}>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
-            </ul>
             <Menu onClick={handleToggleMenu} className={`lg:hidden ${expanded ? 'hidden' : 'block'}`} />
-            <h1 className="font-[family-name:var(--font-montserrat)] font-bold text-2xl sm:text-3xl lg:text-4xl">Memphis Tech Scene</h1>
+            <h1 className="font-[family-name:var(--font-montserrat)] font-bold text-2xl sm:text-3xl lg:text-4xl z-50">Memphis Tech Scene</h1>
         </nav>
     )
 }
-// <button className="bg-[var(--color-primary-blue)] text-white px-4 py-2 rounded-md font-[family-name:var(--font-montserrat)]">Subscribe</button>
 export default Nav;
