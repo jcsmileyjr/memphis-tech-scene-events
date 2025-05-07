15a8b81252b704c7226bed1e876912d985f419be
JC <jcsmileyjr@gmail.com>
2025-05-07 01:56:47 -0500

    Feat: Initial UI for Teaser component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Nav from "@/components/nav/page";
+import Teaser from "@/components/teaser/page";
 
 export default function Home() {
   return (
     <div className=" min-h-screen pb-20 gap-16 font-[family-name:var(--font-quicksand)]">
       <Nav />
-      <main className="flex flex-col px-8 gap-8">Hello World: Testing, the world is full of hope and passion.</main>
+      <main className="flex flex-col px-8 gap-8">
+        <Teaser />
+      </main>
     </div>
   );
 }
---
+++ src/components/teaser/page.tsx
@@ -0,0 +1,20 @@
+
+
+const Teaser = () => {
+    return (
+        <section>
+            <div className="text-[var(--color-primary-blue)] uppercase font-bold">
+                Single source 
+                <span className="font-base lowercase text-black px-2">of</span> 
+                truth 
+            </div>
+            <div className="font-base lowercase text-black pl-8">
+                for upcoming
+                <span className="text-[var(--color-primary-blue)] uppercase font-bold px-2">tech events</span>
+                in Memphis
+            </div>
+        </section>
+    );
+}
+
+export default Teaser;
