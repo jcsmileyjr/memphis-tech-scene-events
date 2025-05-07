00d8af94e9defa5e63d80ecb588777303e227f0b
JC <jcsmileyjr@gmail.com>
2025-05-07 02:17:04 -0500

    WIP Refactor: Highlight current nav element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Teaser from "@/components/teaser/page";
 export default function Home() {
   return (
     <div className=" min-h-screen pb-20 gap-16 font-[family-name:var(--font-quicksand)]">
-      <Nav />
+      <Nav pageLocation="Home" />
       <main className="flex flex-col px-4 gap-8">
         <Teaser />
       </main>
--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -3,7 +3,7 @@ import { Menu } from 'lucide-react';
 import {useState} from 'react';
 import { useMediaQuery } from 'react-responsive';
 
-const Nav = () => {
+const Nav = ({pageLocation}: {pageLocation: string}) => {
     const [expanded, setExpanded] = useState(false);
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1280px)' });
 
@@ -16,8 +16,8 @@ const Nav = () => {
     return (
         <nav className={`flex flex-row px-4 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
             <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-12 sm:pt-14 ml-4' : 'hidden'}`}>
-                <li className=" cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline">Home</li>
-                <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Events</li>
+                <li className={`${pageLocation==='Home' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Home</li>
+                <li className={`${pageLocation==='Events' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)] hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Events</li>
                 <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">About</li>
                 <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Sponsors</li>
                 <li className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Collaborator</li>
