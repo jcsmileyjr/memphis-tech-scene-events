8b1f5714d0bc80a113470b4b10db51ec665006e4
JC <jcsmileyjr@gmail.com>
2025-05-07 01:29:48 -0500

    Style: fix hidden navbar on desktop screens

--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -1,26 +1,30 @@
 "use client";
 import { Menu } from 'lucide-react';
 import {useState} from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const Nav = () => {
     const [expanded, setExpanded] = useState(false);
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1280px)' });
 
     const handleToggleMenu = () => {
-        setExpanded(!expanded);
+        if (isTabletOrMobile) {
+            setExpanded(!expanded);
+        }
     }
 
     return (
         <nav className={`flex flex-row px-8 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
-            <ul onClick={handleToggleMenu} className={`hideen lg:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-8 sm:pt-10 ml-4' : 'hidden'}`}>
-                <li className="bg-white lg:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)]">Home</li>
-                <li className="menu-item bg-white lg:bg-transparent">Events</li>
-                <li className="menu-item bg-white lg:bg-transparent">About</li>
-                <li className="menu-item bg-white lg:bg-transparent">Sponsors</li>
-                <li className="menu-item bg-white lg:bg-transparent">Collaborator</li>
-                <li className="menu-item bg-white lg:bg-transparent">Testimonials</li>
+            <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-8 sm:pt-10 ml-4' : 'hidden'}`}>
+                <li className="bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)]">Home</li>
+                <li className="menu-item bg-white xl:bg-transparent">Events</li>
+                <li className="menu-item bg-white xl:bg-transparent">About</li>
+                <li className="menu-item bg-white xl:bg-transparent">Sponsors</li>
+                <li className="menu-item bg-white xl:bg-transparent">Collaborator</li>
+                <li className="menu-item bg-white xl:bg-transparent">Testimonials</li>
             </ul>
-            <Menu onClick={handleToggleMenu} className={`lg:hidden ${expanded ? 'hidden' : 'block'}`} />
-            <h1 className="font-[family-name:var(--font-montserrat)] font-bold text-2xl sm:text-3xl lg:text-4xl z-50">Memphis Tech Scene</h1>
+            <Menu onClick={handleToggleMenu} className={`xl:hidden ${expanded ? 'hidden' : 'block'}`} />
+            <h1 className="font-[family-name:var(--font-montserrat)] font-bold text-2xl sm:text-3xl xl:text-4xl z-50">Memphis Tech Scene</h1>
         </nav>
     )
 }
