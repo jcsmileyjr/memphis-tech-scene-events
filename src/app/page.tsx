f961867b4cd639a86a91591707f43774be503a70
JC <jcsmileyjr@gmail.com>
2025-05-07 01:04:22 -0500

    WIP: Styling issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Nav from "@/components/nav/page";
 
 export default function Home() {
   return (
-    <div className=" min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-quicksand)]">
+    <div className=" min-h-screen pb-20 gap-16 font-[family-name:var(--font-quicksand)]">
       <Nav />
-      <main className="flex flex-col gap-8">Hello World</main>
+      <main className="flex flex-col px-8 gap-8">Hello World: Testing, the world is full of hope and passion.</main>
     </div>
   );
 }
--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -10,14 +10,14 @@ const Nav = () => {
     }
 
     return (
-        <nav className={`flex flex-row  ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
-            <ul onClick={handleToggleMenu} className={`lg:hidden lg:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-white pt-8' : 'hidden'}`}>
-                <li className="text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)]">Home</li>
-                <li className="menu-item">Events</li>
-                <li className="menu-item">About</li>
-                <li className="menu-item">Sponsors</li>
-                <li className="menu-item">Collaborator</li>
-                <li className="menu-item">Testimonials</li>
+        <nav className={`flex flex-row px-8 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
+            <ul onClick={handleToggleMenu} className={`hideen lg:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-8 sm:pt-10 ml-4' : 'hidden'}`}>
+                <li className="bg-white lg:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)]">Home</li>
+                <li className="menu-item bg-white lg:bg-transparent">Events</li>
+                <li className="menu-item bg-white lg:bg-transparent">About</li>
+                <li className="menu-item bg-white lg:bg-transparent">Sponsors</li>
+                <li className="menu-item bg-white lg:bg-transparent">Collaborator</li>
+                <li className="menu-item bg-white lg:bg-transparent">Testimonials</li>
             </ul>
             <Menu onClick={handleToggleMenu} className={`lg:hidden ${expanded ? 'hidden' : 'block'}`} />
             <h1 className="font-[family-name:var(--font-montserrat)] font-bold text-2xl sm:text-3xl lg:text-4xl z-50">Memphis Tech Scene</h1>
