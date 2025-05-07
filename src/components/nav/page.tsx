e3fedb99d24bccda0b2e45d81599dba6e459562a
JC <jcsmileyjr@gmail.com>
2025-05-07 01:33:49 -0500

    Style: Add extra whitespace around navbar

--- src/components/nav/page.tsx
+++ src/components/nav/page.tsx
@@ -14,8 +14,8 @@ const Nav = () => {
     }
 
     return (
-        <nav className={`flex flex-row px-8 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
-            <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-8 sm:pt-10 ml-4' : 'hidden'}`}>
+        <nav className={`flex flex-row px-8 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative items-start justify-end' : 'items-center justify-between'}`}>
+            <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-[family-name:var(--font-quicksand)] ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-12 sm:pt-14 ml-4' : 'hidden'}`}>
                 <li className="bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-[family-name:var(--font-montserrat)]">Home</li>
                 <li className="menu-item bg-white xl:bg-transparent">Events</li>
                 <li className="menu-item bg-white xl:bg-transparent">About</li>
