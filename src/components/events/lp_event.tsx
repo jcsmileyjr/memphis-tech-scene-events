bd931655f9bb7926bf6563ff2f0d981988ae3821
JC <jcsmileyjr@gmail.com>
2025-05-08 17:44:06 -0500

    Style: Whitespace within events on landing page in mobile view.

--- src/components/events/lp_event.tsx
+++ src/components/events/lp_event.tsx
@@ -8,7 +8,7 @@ interface EventProps {
 
 const LP_Event = ({ name, date, description }: EventProps) => {
     return (
-        <div className='flex-1 h-full border-[#D9D9D9] border p-4 flex flex-col justify-between font-quicksand font-normal rounded-lg shadow-md'>
+        <div className='flex-1 h-full border-[#D9D9D9] border p-4 flex flex-col gap-4 lg:gap-0 justify-between font-quicksand font-normal rounded-lg shadow-md'>
             <BugPlay size={48} />
             <p className='font-bold text-normal xl:text-xl font-montserrat'>{name}</p>
             <p>{date}</p>
