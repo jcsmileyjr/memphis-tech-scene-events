7760da061c0a63a4a81f7b2d25997bc0406fdeb7
JC <jcsmileyjr@gmail.com>
2025-05-09 04:06:10 -0500

    Refactor: Mobile Responseness of Teaser content

--- src/components/teaser/teaser.tsx
+++ src/components/teaser/teaser.tsx
@@ -8,8 +8,8 @@ const Teaser = () => {
                 <span className="font-normal lowercase text-black px-2">of</span> 
                 truth 
             </div>
-            <div className="font-base lowercase text-black pl-4 sm:pl-8">
-                for upcoming
+            <div className="font-base lowercase text-black pl-24 sm:pl-24">
+                for<span className="hidden sm:inline">upcoming</span>
                 <span className="text-[var(--color-primary-blue)] uppercase font-bold px-2">tech 
                     <span className="hidden sm:inline pl-2">events</span>
                 </span>
