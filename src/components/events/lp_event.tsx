8ecf0bdc4ab872b20087c9723e60b48765087e3d
JC <jcsmileyjr@gmail.com>
2025-05-09 03:46:43 -0500

    Style: Event consistant font sizes

--- src/components/events/lp_event.tsx
+++ src/components/events/lp_event.tsx
@@ -9,9 +9,9 @@ interface EventProps {
 
 const LP_Event = ({ name, date, description, iconName }: EventProps) => {
     return (
-        <div className='flex-1 h-full border-[#D9D9D9] border p-4 flex flex-col gap-4 lg:gap-0 justify-between font-quicksand font-normal rounded-lg shadow-md'>
+        <div className='flex-1 h-full border-[#D9D9D9] border p-4 flex flex-col gap-4 lg:gap-0 justify-between font-quicksand font-normal text-normal xl:text-xl rounded-lg shadow-md'>
             <LP_Event_Icon iconName={iconName} />
-            <p className='font-bold text-normal xl:text-xl font-montserrat'>{name}</p>
+            <p className='font-bold font-montserrat'>{name}</p>
             <p>{date}</p>
             <p>{description}</p>
         </div>
