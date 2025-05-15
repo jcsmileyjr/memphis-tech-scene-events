84bf1e8178738197d563af3abbbc71d5ab60bf5f
JC <jcsmileyjr@gmail.com>
2025-05-15 12:08:27 -0500

    Style: Change nav into a full width bar instead of a vertical bar
    Solving issue of the vertical nav looking out of place.

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -17,19 +17,19 @@ const Nav = () => {
 
     return (
         <nav aria-label="Main Navigation" className={`flex flex-row justify-between items-center px-4 xl:px-12 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative' : ' '}`}>
-            <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-quicksand ${expanded ? ' absolute top-0 left-0 z-50 bg-transparent pt-12 sm:pt-14 ml-4' : 'hidden'}`}>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Home</li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/events' ? 'cursor-pointer bg-white xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Events</li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">About</li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Sponsors</li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Collaborator</li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-white xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Testimonials</li>
+            <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-quicksand ${expanded ? ' absolute top-0 left-0 z-50 bg-[var(--nav-backgroundcolor-dark)] pt-14 pb-8 w-full flex flex-col justify-center items-center' : 'hidden'}`}>
+                <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/' ? 'cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Home</li>
+                <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/events' ? 'cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>Events</li>
+                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">About</li>
+                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Sponsors</li>
+                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Collaborator</li>
+                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">Testimonials</li>
             </ul>
             <button
                 type="button"
                 onClick={handleToggleMenu}
                 aria-label="Toggle navigation menu"
-                className="xl:hidden"
+                className="z-50 xl:hidden"
             >
                 <Menu />
             </button>
