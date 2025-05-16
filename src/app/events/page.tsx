0fb7757639ecc59180551a611b6bcb9af6b1f283
JC <jcsmileyjr@gmail.com>
2025-05-16 09:15:14 -0500

    Refactor: Add logo to teaser section of Events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,12 +1,22 @@
 import Nav from "@/components/nav/nav";
 import Teaser from "@/components/teaser/teaser";
+import Image from "next/image";
 
 const EventsPage = () => {
     return (
         <div className="min-h-screen gap-16 font-quicksand">
             <Nav />
             <main className="flex flex-col">
-                <Teaser />
+                <div className="flex flex-row px-4 xl:px-12">
+                    <Image
+                        src="/images/coder-logo.png"
+                        alt="Logo"
+                        width={1000}
+                        height={500}
+                        className="w-20 h-20 object-cover hidden sm:block"
+                    />
+                    <Teaser />
+                </div>
             </main>
         </div>
     );
