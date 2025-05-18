cf2e6d55cd2df27487385894eacf1d85316d9da6
JC <jcsmileyjr@gmail.com>
2025-05-18 12:43:44 -0500

    Style: Update Nav bar to be responsive in ultra size screens

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -17,30 +17,32 @@ const Nav = () => {
     }
 
     return (
-        <nav aria-label="Main Navigation" className={`flex flex-row justify-between items-center px-4 xl:px-12 py-2 bg-[var(--nav-backgroundcolor)] ${expanded ? 'relative' : ' '}`}>
-            <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-quicksand  ${expanded ? ' absolute top-0 left-0 z-50 bg-[var(--nav-backgroundcolor-dark)] pt-14 pb-8 px-8 w-full flex flex-col justify-center items-center' : 'hidden'}`}>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/' ? 'cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>
-                    <Link href="/">Home</Link>
-                </li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/events' ? 'cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>
-                    <Link href="/events">Events</Link>
-                </li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-not-allowed bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">
-                    <Link href="/#sponsors">Sponsors</Link>
-                </li>
-                <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-not-allowed bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">
-                    <Link href="/#partners">Partners</Link>
-                </li>
-            </ul>
-            <button
-                type="button"
-                onClick={handleToggleMenu}
-                aria-label="Toggle navigation menu"
-                className="z-50 xl:hidden"
-            >
-                <Menu />
-            </button>
-            <h1 className="font-montserrat font-bold text-xl sm:text-3xl xl:text-4xl z-50">Memphis Tech Scene</h1>
+        <nav aria-label="Main Navigation" className='bg-[var(--nav-backgroundcolor)]'>
+            <div className={`flex flex-row justify-between items-center px-4 xl:px-12 py-2 2xl:mx-auto 2xl:w-full 2xl:max-w-[2300px] ${expanded ? 'relative' : ' '}`}>
+                <ul onClick={handleToggleMenu} className={` xl:flex flex-row flex-wrap gap-4 text-xl font-quicksand  ${expanded ? ' absolute top-0 left-0 z-50 bg-[var(--nav-backgroundcolor-dark)] pt-14 pb-8 px-8 w-full flex flex-col justify-center items-center' : 'hidden'}`}>
+                    <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/' ? 'cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li aria-current={pageLocation === '/' ? 'page' : undefined} className={`${pageLocation==='/events' ? 'cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent text-[var(--color-primary-blue)] font-bold font-montserrat hover:text-[var(--color-primary-blue)] hover:underline':'menu-item cursor-pointer bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline'}`}>
+                        <Link href="/events">Events</Link>
+                    </li>
+                    <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-not-allowed bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">
+                        <Link href="/#sponsors">Sponsors</Link>
+                    </li>
+                    <li aria-current={pageLocation === '/' ? 'page' : undefined} className="menu-item cursor-not-allowed bg-[var(--nav-backgroundcolor-dark)] xl:bg-transparent hover:text-[var(--color-primary-blue)] hover:underline">
+                        <Link href="/#partners">Partners</Link>
+                    </li>
+                </ul>
+                <button
+                    type="button"
+                    onClick={handleToggleMenu}
+                    aria-label="Toggle navigation menu"
+                    className="z-50 xl:hidden"
+                >
+                    <Menu />
+                </button>
+                <h1 className="font-montserrat font-bold text-xl sm:text-3xl xl:text-4xl z-50">Memphis Tech Scene</h1>
+            </div>
         </nav>
     )
 }
