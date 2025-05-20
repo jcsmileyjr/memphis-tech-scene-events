e44465d52483710aae677d16187da892164bf662
JC <jcsmileyjr@gmail.com>
2025-05-19 20:11:03 -0500

    Refactor: Fix code format issues

--- src/components/teaser/teaser.tsx
+++ src/components/teaser/teaser.tsx
@@ -2,7 +2,7 @@ const Teaser = () => {
     return (
         <section
             aria-label="Teaser Section"
-            className="flex flex-col gap-2 m-auto py-4 text-xl"
+            className="flex flex-col gap-2 mx-auto py-4 text-xl"
         >
             <div className="text-[var(--color-primary-blue)] uppercase font-bold">
                 Single source
@@ -11,7 +11,7 @@ const Teaser = () => {
                 </span>
                 truth
             </div>
-            <div className="font-base lowercase text-black text-center sm:text-left sm:pl-24">
+            <div className="font-normal lowercase text-black text-center sm:text-left sm:pl-24">
                 for<span className="hidden sm:inline"> upcoming</span>
                 <span className="text-[var(--color-primary-blue)] uppercase font-bold px-2">
                     tech
